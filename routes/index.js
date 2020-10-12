fce0a64b1915ebaef20a8ce63ba36506106bf4cb
Tirth Shah <shahtirth229@gmail.com>
2020-10-12 14:36:35 +0300

    Added Homepage, About us page, images, css, and js files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+// GET about page
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
 });
 
 module.exports = router;
